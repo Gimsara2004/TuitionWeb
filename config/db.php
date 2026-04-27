91b233d3d2b7708c94a0be5ae8a2ef1fb79faeaa
Gimsara <gimsarapieris6@gmail.com>
2026-04-27 19:27:33 +0530

    add dp

--- config/db.php
+++ config/db.php
@@ -8,14 +8,6 @@ if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.
 
 } else {
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-    // ── PRODUCTION ────────────────────────
-    // Update these when you deploy to a live host
-=======
->>>>>>> beb8c16 (change the town)
->>>>>>> c115f74 (update index)
     $host = "sql313.infinityfree.com";
     $user = "if0_41753756";
     $pass = "veqq2TXu9xQd";
